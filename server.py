0ba3d0ecc40748539cbdefc8fe5ed7759e203626
smitpatel2 <98126697+smitpatel2@users.noreply.github.com>
2023-03-10 16:35:46 -0700

    changed a few things added imports

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 # Copyright 2013 Abram Hindle
 # 
+# Added code by Smit Patel
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -22,7 +23,7 @@
 
 
 import flask
-from flask import Flask, request
+from flask import Flask, redirect, request, jsonify
 import json
 app = Flask(__name__)
 app.debug = True
